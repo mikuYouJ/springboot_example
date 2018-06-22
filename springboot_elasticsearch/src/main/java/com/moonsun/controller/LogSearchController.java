107c071e028313209bc2060069e8c9191238de1d
mikuYouJ <826104943@qq.com>
2018-06-22 15:58:44 +0800

    commit

--- springboot_elasticsearch/src/main/java/com/moonsun/controller/LogSearchController.java
+++ springboot_elasticsearch/src/main/java/com/moonsun/controller/LogSearchController.java
@@ -1,10 +1,24 @@
 package com.moonsun.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by 82610 on 2018/6/13.
  */
 @RestController
 public class LogSearchController {
+
+    @GetMapping("/index")
+    public ModelAndView index(){
+        Map<String,Object> data = new HashMap<String,Object>();
+        data.put("user",1);
+        return new ModelAndView("index",data);
+    }
+
 }
