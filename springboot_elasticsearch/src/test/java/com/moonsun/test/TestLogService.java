603820691f8480dc18c202878a09a18b35705b01
mikuYouJ <826104943@qq.com>
2018-07-04 10:17:55 +0800

    commit

--- springboot_elasticsearch/src/test/java/com/moonsun/test/TestLogService.java
+++ springboot_elasticsearch/src/test/java/com/moonsun/test/TestLogService.java
@@ -1,5 +1,8 @@
 package com.moonsun.test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.moonsun.Application;
 import com.moonsun.service.ILogService;
 import com.moonsun.service.impl.LogServiceImpl;
@@ -34,5 +37,16 @@ public class TestLogService {
          params.put("beginTime","2018-07-02 00:00:00");
          params.put("endTime","2018-07-03 23:00:00");
          logService.queryLogs(params);
+    }
+
+    @Test
+    public void test(){
+       String jsonSrt = "{\"total\":28,\"row\":[{\"@timestamp\":\"2018-07-03T00:44:05.589Z\",\"@version\":\"1\",\"type\":\"szAirlinesWebSystem\",\"message\":\"{\\\"@source\\\":\\\"szAirlinesSystem\\\",\\\"@source_host\\\":\\\"pp-app01\\\",\\\"@source_path\\\":null,\\\"@type\\\":\\\"szAirlinesSystem\\\",\\\"@tags\\\":[\\\"szAirlinesSystem\\\"],\\\"@message\\\":\\\"==> Parameters: 118070225950(Long)\\\",\\\"@timestamp\\\":\\\"2018-07-03T08:44:04.424+0800\\\",\\\"@fields\\\":{\\\"logger\\\":\\\"com.skyecho.shenzhenairlines.order.vo.AirBookLineVO.selectByResId\\\",\\\"level\\\":\\\"DEBUG\\\",\\\"thread\\\":\\\"http-bio-18080-exec-77\\\",\\\"level\\\":\\\"DEBUG\\\",\\\"location\\\":{\\\"class\\\":\\\"org.apache.ibatis.logging.jdbc.BaseJdbcLogger\\\",\\\"method\\\":\\\"debug\\\",\\\"file\\\":\\\"BaseJdbcLogger.java\\\",\\\"line\\\":\\\"132\\\"}}}\"}]}";
+        JSONObject jsonObject = JSON.parseObject(jsonSrt);
+        JSONArray jsonArray = (JSONArray) jsonObject.get("row");
+        JSONObject rowobj = (JSONObject) jsonArray.get(0);
+        String messgae = (String) rowobj.get("message");
+        JSONObject messgaeJO = JSON.parseObject(messgae);
+        System.out.println(messgaeJO);
     }
 }
